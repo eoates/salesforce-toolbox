1f1f8223779ff9c9f43928b67c66ee98f63c9ce5
Eugene Oates <eugene.oates@gmail.com>
2017-11-07 14:41:14 -0600

    Added inputDate component.

---
+++ src/aura/inputDate/inputDateController.js
@@ -0,0 +1,146 @@
+({
+	/**
+	 * Sets focus to the input element
+	 */
+	focus: function(component, event, helper) {
+		var inputElement = helper.getInputElement(component);
+		if (inputElement) {
+			inputElement.focus();
+		}
+	},
+
+	/**
+	 * Selects the input element
+	 */
+	select: function() {
+		var inputElement = helper.getInputElement(component);
+		if (inputElement) {
+			inputElement.select();
+		}
+	},
+
+	/**
+	 * Handles change to the value attribute
+	 */
+	valueChange: function(component, event, helper) {
+		if (component.ignoreValueChange) {
+			return;
+		}
+		helper.updateInputElementValue(component);
+	},
+
+	/**
+	 * Handles the focusin event of the container element
+	 */
+	containerFocusIn: function(component, event, helper) {
+		// Check the blurTimeoutId property on the component. If it has a value then that means we
+		// just lost focus because focus changed from one element to another within the container
+		// so we just want to clear the timeout and do nothing. Otherwise, if blurTimeoutId has no
+		// value then we are getting focus from some element outside of the datepicker
+		if (component.blurTimeoutId) {
+			clearTimeout(component.blurTimeoutId);
+			component.blurTimeoutId = null;
+			return;
+		}
+
+		helper.fireEvent(component, 'onfocus');
+	},
+
+	/**
+	 * Handles the focusout event of the container element
+	 */
+	containerFocusOut: function(component, event, helper) {
+		// When focus goes from one control to another within the container the onfocusout will be
+		// fired when the current element loses focus and onfocusin will be fired when the new
+		// element gains focus. When onfocusout is called we use a timeout to wait a fraction of a
+		// second to see if another element within the container gains focus. If so then we will
+		// clear the timeout and do nothing. If not, however, then we will fire our own onblur event
+		if (component.blurTimeoutId) {
+			clearTimeout(component.blurTimeoutId);
+		}
+
+		component.blurTimeoutId = setTimeout($A.getCallback(function() {
+			component.blurTimeoutId = null;
+			helper.fireEvent(component, 'onblur');
+		}), 0);
+	},
+
+	/**
+	 * Handles the focus event of the input element
+	 */
+	inputFocus: function(component, event, helper) {
+		if (helper.isDesktop()) {
+			var inputElement = event.target;
+
+			var autoSelect = component.get('v.autoselect');
+			if (autoSelect) {
+				inputElement.select();
+			}
+		} else {
+			helper.fireEvent(component, 'onfocus');
+		}
+	},
+
+	/**
+	 * Handles the blur event of the input element
+	 */
+	inputBlur: function(component, event, helper) {
+		if (helper.isDesktop()) {
+			helper.updateInputElementValue(component);
+		} else {
+			helper.fireEvent(component, 'onblur');
+		}
+	},
+
+	/**
+	 * Handles the change event of the input element
+	 */
+	inputChange: function(component, event, helper) {
+		var inputElement = event.target;
+		helper.setValue(component, inputElement.value);
+	},
+
+	/**
+	 * Handles the click event of the trigger element
+	 */
+	triggerClick: function(component, event, helper) {
+		helper.open(component, true);
+	},
+
+	/**
+	 * Handles the select event of the datepicker
+	 */
+	datepickerSelect: function(component, event, helper) {
+		var datepicker = event.getSource();
+		var value = datepicker.get('v.value');
+
+		helper.setValue(component, value);
+		helper.close(component, true);
+	},
+
+	/**
+	 * Handles the change event of the datepicker
+	 */
+	datepickerChange: function(component, event, helper) {
+	},
+
+	/**
+	 * Handles the cancel event of the datepicker
+	 */
+	datepickerCancel: function(component, event, helper) {
+		helper.close(component, true);
+	},
+
+	/**
+	 * Handles the focus event of the datepicker
+	 */
+	datepickerFocus: function(component, event, helper) {
+	},
+
+	/**
+	 * Handles the blur event of the datepicker
+	 */
+	datepickerBlur: function(component, event, helper) {
+		helper.close(component);
+	}
+})
---
+++ src/aura/inputDate/inputDateHelper.js
@@ -0,0 +1,299 @@
+({
+	valueFormat: 'yyyy-MM-dd',
+
+	/**
+	 * Returns true if the application running on a desktop browser. This method uses the $Browser
+	 * global variable to get the device type
+	 *
+	 * @returns {boolean} true if the application is running on a desktop browser or false if it
+	 *                    is running on a phone or tablet
+	 */
+	isDesktop: function() {
+		var formFactor = $A.get('$Browser.formFactor');
+		return (formFactor === 'DESKTOP');
+	},
+
+	/**
+	 * Returns true if the application is running on a phone or a table. This method will always
+	 * return the opposite of isDesktop()
+	 *
+	 * @returns {boolean} true if the application is running on a phone or a table; otherwise, false
+	 */
+	isMobile: function() {
+		return !this.isDesktop();
+	},
+
+	/**
+	 * Removes leading and trailing white space characters from a string. If the specified value is
+	 * null or undefined then an empty string will be returned. If the value is not a string then it
+	 * will be converted to a string and then trimmed
+	 *
+	 * @param {*} value the value to be trimmed
+	 * @returns {string} the trimmed string
+	 */
+	trim: function(value) {
+		if ($A.util.isUndefinedOrNull(value)) {
+			value = '';
+		} else {
+			if (!this.isString(value)) {
+				value = '' + value;
+			}
+			value = value.replace(/^\s+|\s+$/g, '');
+		}
+		return value;
+	},
+
+	/**
+	 * Converts the value to a Date if possible. If the value cannot be converted to a Date then
+	 * null is returned
+	 *
+	 * @param {*} value the value to convert
+	 * @returns {Date} the converted date or null if the value could not be converted to a Date
+	 */
+	toDate: function(value) {
+		if ($A.util.isUndefinedOrNull(value)) {
+			value = null;
+		} else if (this.isDate(value)) {
+			value = new Date(value.getFullYear(), value.getMonth(), value.getDate());
+		} else if (this.isNumber(value)) {
+			value = new Date(value);
+		} else {
+			value = this.trim(value);
+			if (value !== '') {
+				try {
+					value = $A.localizationService.parseDateTime(value);
+				} catch (e) {
+					value = null;
+				}
+			} else {
+				value = null;
+			}
+		}
+		return value;
+	},
+
+	/**
+	 * Returns true if the value is a string
+	 *
+	 * @param {*} vaue the value to test
+	 * @returns {boolean} true if the value is a string; otherwise, false
+	 */
+	isString: function(value) {
+		return (typeof value === 'string');
+	},
+
+	/**
+	 * Returns true if the value is a number. This method does not treat special NaN or Infinity as
+	 * numbers even though they technically are
+	 *
+	 * @param {*} value the value to test
+	 * @returns {boolean} true if the value is a number; otherwise, false
+	 */
+	isNumber: function(value) {
+		return (typeof value === 'number') && isFinite(value);
+	},
+
+	/**
+	 * Returns true if the value is a date
+	 *
+	 * @param {*} value the value to test
+	 * @returns {boolean} true if the value is a date; otherwise, false
+	 */
+	isDate: function(value) {
+		return (Object.prototype.toString.call(value) === '[object Date]');
+	},
+
+	/**
+	 * Returns true if the dropdown is open
+	 *
+	 * @param {Aura.Component} component the inputDate component
+	 * @returns {boolean} true if the dropdown is open; otherwise, false
+	 */
+	isOpen: function(component) {
+		var container = component.find('container');
+		return $A.util.hasClass(container.getElement(), 'slds-is-open');
+	},
+
+	/**
+	 * Opens the dropdown
+	 *
+	 * @param {Aura.Component} component the inputDate component
+	 * @param {boolean} [focus] true if the datepicker should receive focus
+	 * @returns {void}
+	 */
+	open: function(component) {
+		var datepicker = component.find('datepicker');
+
+		if (!this.isOpen(component)) {
+			datepicker.set('v.value', component.get('v.value'));
+
+			var dropdown = component.find('dropdown');
+			dropdown.getElement().setAttribute('aria-hidden', false);
+
+			var container = component.find('container');
+			$A.util.addClass(container.getElement(), 'slds-is-open');
+		}
+
+		if (focus) {
+			datepicker.focus();
+		}
+	},
+
+	/**
+	 * Closes the dropdown
+	 *
+	 * @param {Aura.Component} component the inputDate component
+	 * @param {boolean} [focus] true if the input element should recieve focus
+	 * @returns {void}
+	 */
+	close: function(component, focus) {
+		if (this.isOpen(component)) {
+			var container = component.find('container');
+			$A.util.removeClass(container.getElement(), 'slds-is-open');
+
+			var dropdown = component.find('dropdown');
+			dropdown.getElement().setAttribute('aria-hidden', true);
+
+			var datepicker = component.find('datepicker');
+			datepicker.set('v.value', null);
+		}
+
+		if (focus) {
+			var inputElement = this.getInputElement(component);
+			inputElement.focus();
+		}
+	},
+
+	/**
+	 * Formats a date using the specified format. If no format is provided then a default format
+	 * will be used
+	 *
+	 * @param {Date} value the date to format
+	 * @param {string} [format] the format
+	 * @returns {string} the formatted date
+	 */
+	formatDate: function(value, format) {
+		if ($A.util.isUndefinedOrNull(value)) {
+			return '';
+		}
+
+		if ($A.util.isEmpty(format)) {
+			format = this.valueFormat;
+		} else {
+			format = '' + format;
+		}
+
+		return $A.localizationService.formatDateTime(value, format);
+	},
+
+	/**
+	 * Returns the value as a formatted string
+	 *
+	 * @param {Aura.Component} component the inputDate component
+	 * @returns {string} the formatted value
+	 */
+	getFormattedValue: function(component) {
+		var value = component.get('v.value');
+		var format = component.get('v.format');
+
+		if (this.isMobile()) {
+			format = this.valueFormat;
+		}
+
+		var strValue = '';
+		if (!$A.util.isUndefinedOrNull(value)) {
+			var dateValue = this.toDate(value);
+			if (dateValue) {
+				strValue = this.formatDate(dateValue, format);
+			} else {
+				strValue = value + '';
+			}
+		}
+
+		return strValue;
+	},
+
+	/**
+	 * Sets the value
+	 *
+	 * @param {Aura.Component} component the inputDate component
+	 * @param {string} value the value
+	 * @returns {void}
+	 */
+	setValue: function(component, value) {
+		// Format the value
+		value = this.trim(value);
+		if (value) {
+			var dateValue = this.toDate(value);
+			if (dateValue) {
+				value = this.formatDate(dateValue);
+			} else {
+				value = null;
+			}
+		}
+
+		// Determine whether the value has changed
+		var oldValue = component.get('v.value');
+		if (value === oldValue) {
+			return;
+		}
+
+		// Updat the value
+		component.ignoreValueChange = true;
+		try {
+			component.set('v.value', value);
+		} finally {
+			component.ignoreValueChange = false;
+		}
+
+		// Update the input element value
+		this.updateInputElementValue(component);
+
+		// Fire the change event
+		this.fireEvent(component, 'onchange', {
+			value: value,
+			oldValue: oldValue
+		});
+	},
+
+	/**
+	 * Returns the input HTML element
+	 *
+	 * @param {Aura.Component} component the inputDate component
+	 * @returns {HTMLElement} the input element
+	 */
+	getInputElement: function(component) {
+		var input = component.find('input');
+		if (input) {
+			return input.getElement();
+		}
+		return null;
+	},
+
+	/**
+	 * Updates the value of the input element so it displays the formatted value
+	 *
+	 * @param {Aura.Component} component the inputDate component
+	 * @returns {void}
+	 */
+	updateInputElementValue: function(component) {
+		var inputElement = this.getInputElement(component);
+		if (inputElement) {
+			inputElement.value = this.getFormattedValue(component);
+		}
+	},
+
+	/**
+	 * Fire the named event
+	 *
+	 * @param {Aura.Component} component the datepicker component
+	 * @param {string} name the event name
+	 * @param {object} args optional event arguments
+	 * @returns {void}
+	 */
+	fireEvent: function(component, name, args) {
+		var event = component.getEvent(name);
+		event.setParam('arguments', args || {});
+		event.fire();
+	}
+})
