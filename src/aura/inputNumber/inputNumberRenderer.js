6a878390c5ee7ff24e65132d2dbff965c52f3205
Eugene Oates <eugene.oates@gmail.com>
2017-11-08 15:55:21 -0600

    Added comments to renderer methods for inputText and inputNumber components.

--- src/aura/inputNumber/inputNumberRenderer.js
+++ src/aura/inputNumber/inputNumberRenderer.js
@@ -1,9 +1,15 @@
 ({
+	/**
+	 * Invoked after the component has been rendered for the first time
+	 */
 	afterRender: function(component, helper) {
 		this.superAfterRender();
 		helper.renderInputElement(component);
 	},
 
+	/**
+	 * Invoked each time the component is rerendered
+	 */
 	rerender: function(component, helper) {
 		this.superRerender();
 		helper.renderInputElement(component);
--- src/aura/inputText/inputTextRenderer.js
+++ src/aura/inputText/inputTextRenderer.js
@@ -1,4 +1,7 @@
 ({
+	/**
+	 * Invoked after the component has been rendered for the first time
+	 */
 	afterRender: function(component, helper) {
 		this.superAfterRender();
 		helper.updateInputElementValue(component);
