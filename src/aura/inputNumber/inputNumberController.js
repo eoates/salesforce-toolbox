751ea4523bd818aa96accd30a4f3ba7257b05309
Eugene Oates <eugene.oates@gmail.com>
2017-11-13 01:05:01 -0600

    Refactor inputNumber to use inputNumberBehavior.

--- src/aura/inputNumber/inputNumberController.js
+++ src/aura/inputNumber/inputNumberController.js
@@ -42,23 +42,7 @@
 	 */
 	inputFocus: function(component, event, helper) {
 		if (helper.utils.isDesktop()) {
-			var inputElement = event.target;
-
-			var selectionStart = inputElement.selectionStart;
-			var selectionEnd = inputElement.selectionEnd;
-			var selectionLength = selectionEnd - selectionStart;
-
-			var oldValue = inputElement.value;
-
-			var value = oldValue.replace(/,/g, '');
-			value = helper.utils.trim(value);
-
-			inputElement.value = value;
-
-			var autoSelect = component.get('v.autoselect');
-			if ((selectionLength === oldValue.length) || autoSelect) {
-				inputElement.select();
-			}
+			helper.performBehaviorAction(component, event, 'onFocus');
 		}
 
 		helper.fireEvent(component, 'onfocus');
@@ -68,7 +52,7 @@
 	 * Handles the blur event of the input element. Applies formatting
 	 */
 	inputBlur: function(component, event, helper) {
-		helper.updateInputElement(component);
+		helper.performBehaviorAction(component, event, 'onBlur');
 		helper.fireEvent(component, 'onblur');
 	},
 
@@ -77,45 +61,10 @@
 	 * functionality
 	 */
 	inputKeyDown: function(component, event, helper) {
-		var inputElement = event.target;
-
-		var format = helper.getFormat(component);
-		var step = format.step;
-		var stepMin = format.stepMin;
-
-		var value = helper.parseNumber(inputElement.value, format);
-		if (!helper.utils.isNumber(value)) {
-			value = 0;
-		}
-
-		var newValue = (value - stepMin) - ((value - stepMin) % step) + stepMin;
-
-		var which = event.keyCode || event.which || 0;
-		if (which === 38) {
-			// Arrow up
-			if ((newValue + step) <= format.max) {
-				newValue += step;
-			}
-		} else if (which === 40) {
-			// Arrow down
-			if ((newValue >= value) && ((newValue - step) >= format.min)) {
-				newValue -= step;
-			}
-		} else {
-			// We don't care about other keys
-			return;
-		}
-
-		newValue = newValue.toFixed(format.scale);
-		newValue = parseFloat(newValue);
-
-		var changed = helper.setValue(component, newValue);
-		helper.updateInputElement(component, true);
+		var changed = helper.performBehaviorAction(component, event, 'onKeyDown');
 		if (changed) {
 			helper.fireEvent(component, 'onchange');
 		}
-
-		event.preventDefault();
 	},
 
 	/**
@@ -128,75 +77,14 @@
 			return;
 		}
 
-		var inputElement = event.target;
-		var which = event.keyCode || event.which || 0;
-
-		var format = helper.getFormat(component);
-		var allowNegative = format.min < 0;
-
-		var value = inputElement.value;
-		var selectionStart = inputElement.selectionStart;
-		var selectionEnd = inputElement.selectionEnd;
-		var selectionLength = selectionEnd - selectionStart;
-
-		if (which === 45) {
-			// 45 = minus
-			// If negative values are not allowed or if we are not at the first char then do not
-			// allow the key
-			if (!allowNegative || (selectionStart !== 0)) {
-				event.preventDefault();
-			}
-		} else if (which === 46) {
-			// 46 = period
-			// If one or more characters are selected then we remove the selected substring from
-			// the value as it will be overwritten with the period
-			if (selectionLength > 0) {
-				var left = value.substr(0, selectionStart);
-				var right = value.substr(selectionEnd);
-				value = left + right;
-			}
-
-			// Make sure the value does not already contain a period
-			if (value.indexOf('.') >= 0) {
-				event.preventDefault();
-			}
-		} else if ((which >= 48) && (which <= 57)) {
-			// 0-9 are allowed
-		}
-		else {
-			// All other characters
-			var allowChar = false;
-
-			// Check for special multipliers, but only at the very end and only if a multiplier
-			// shortcut is not already present
-			var multiplier = helper.hasMultiplier(value);
-			if (
-				!multiplier
-				&& (value.length > 0)
-				&& ((selectionStart + selectionLength) === value.length)
-			) {
-				allowChar = helper.isMultiplier(String.fromCharCode(which));
-			}
-
-			// If the character is not allowed then call preventDefault() on the event to keep it
-			// from being entered into the input
-			if (!allowChar) {
-				event.preventDefault();
-			}
-		}
+		helper.performBehaviorAction(component, event, 'onKeyPress');
 	},
 
 	/**
 	 * Handles the change event of the input element. Updates the component value
 	 */
 	inputChange: function(component, event, helper) {
-		var inputElement = event.target;
-
-		var format = helper.getFormat(component);
-		var value = helper.parseNumber(inputElement.value, format);
-
-		var changed = helper.setValue(component, value);
-		helper.updateInputElement(component);
+		var changed = helper.performBehaviorAction(component, event, 'onChange');
 		if (changed) {
 			helper.fireEvent(component, 'onchange');
 		}
--- src/aura/inputNumber/inputNumberHelper.js
+++ src/aura/inputNumber/inputNumberHelper.js
@@ -1,10 +1,4 @@
 ({
-	minPrecision: 1,
-	maxPrecision: 15,
-	minScale: 0,
-	maxScale: 14,
-	multipliers: [ 'k', 'm', 'b', 't' ],
-
 	/**
 	 * Imports modules used by the component
 	 *
@@ -19,239 +13,42 @@
 	},
 
 	/**
-	 * Converts a value to a number. If the specified value cannot be converted to a number then
-	 * defaultValue is returned
+	 * Performs a specified behavior action
 	 *
-	 * @param {*}      value        - the value to convert
-	 * @param {number} defaultValue - the number to return if value cannot be converted
-	 *
-	 * @return {number} the converted value as a number
+	 * @param  {Aura.Component} component the inputNumber component
+	 * @param  {Event} event the event object
+	 * @param  {string} name the name of the action to perform
+	 * @return {*} varies depending on the action performed
 	 */
-	toNumber: function(value, defaultValue) {
-		value = this.utils.asNumber(value);
-		return this.utils.isNumber(value) ? value : defaultValue;
-	},
+	performBehaviorAction: function(component, event, name) {
+		var inputElement = this.getInputElement(component);
+		var behavior = component.find('behavior').getModule();
+		var action = behavior[name];
 
-	/**
-	 * Similar to the toNumber() method, but the return value is an integer
-	 *
-	 * @param {*}      value        - the value to convert
-	 * @param {number} defaultValue - the number to return if value cannot be converted
-	 *
-	 * @return {number} the converted value as an integer
-	 */
-	toInteger: function(value, defaultValue) {
-		return this.utils.asInteger(this.toNumber(value, defaultValue));
-	},
-
-	/**
-	 * Gets the precision. The precision is the maximum number of digits allowed in the number. Use
-	 * this method to get the precision instead of reading the component's precision attribute
-	 * directly as this method ensures that the returned value is within a vaid range
-	 *
-	 * @param {Aura.Component} component - the inputNumber component
-	 *
-	 * @return {number} the precision
-	 */
-	getPrecision: function(component) {
-		var precision = component.get('v.precision');
-		precision = this.toInteger(precision, this.maxPrecision);
-		precision = this.utils.minmax(precision, this.minPrecision, this.maxPrecision);
-		return precision;
-	},
-
-	/**
-	 * Gets the scale. The scale is the number of digits allowed to the right of the decimal. Use
-	 * this method to get the scale instead of reading the component's scale attribute directly as
-	 * this method ensures that the returned value is within a valid range
-	 *
-	 * @param {Aura.Component} component - the inputNumber component
-	 * @param {number}         precision - the maximum number of digits in the number
-	 *
-	 * @return {number} the scale
-	 */
-	getScale: function(component, precision) {
-		var scale = component.get('v.scale');
-		scale = this.toInteger(scale, this.minScale);
-		scale = this.utils.minmax(scale, this.minScale, this.maxScale);
-		scale = Math.min(scale, precision - 1);
-		return scale;
-	},
-
-	/**
-	 * Returns the minimum and maximum possible values for the given precision and scale
-	 *
-	 * @param {number} precision - the maximum number of digits in the number
-	 * @param {number} scale     - the number of digits to the right of the decimal
-	 *
-	 * @return {Object} an object containing 2 properties: min and max
-	 */
-	getRange: function(precision, scale) {
-		var max = (Math.pow(10, precision) - 1) / Math.pow(10, scale);
-		var min = -max;
-		return {
-			min: min,
-			max: max
-		};
-	},
-
-	/**
-	 * Gets the minimum value
-	 *
-	 * @param {Aura.Component} component - the inputNumber component
-	 * @param {Object}         range     - an object containing the valid number range
-	 *
-	 * @return {number} the minimum value
-	 */
-	getMin: function(component, range) {
-		var min = component.get('v.min');
-		min = this.toNumber(min, range.min);
-		min = this.utils.minmax(min, range.min, range.max);
-		return min;
-	},
-
-	/**
-	 * Gets the maximium value
-	 *
-	 * @param {Aura.Component} component - the inputNumber component
-	 * @param {Object}         range     - an object containing the valid number range
-	 *
-	 * @return {number} the maximum value
-	 */
-	getMax: function(component, range) {
-		var max = component.get('v.max');
-		max = this.toNumber(max, range.max);
-		max = this.utils.minmax(max, range.min, range.max);
-		return max;
-	},
-
-	/**
-	 * Gets the amount by which the component will increment/decrement when the user presses the
-	 * ARROW UP and ARROW DOWN keys respectively
-	 *
-	 * @param {Aura.Component} component - the inputNumber component
-	 * @param {number}         scale     - number of digits to the right of the decimal
-	 *
-	 * @return {number} the step value
-	 */
-	getStep: function(component, scale) {
-		var step = component.get('v.step');
-		step = this.toNumber(step, 1);
-		if (step < 0) {
-			step = 1;
-		} else {
-			step = Math.max(step, 1 / Math.pow(10, scale));
-		}
-		return step;
-	},
-
-	/**
-	 * Returns an object containing information about the number format
-	 *
-	 * @param {Aura.Component} component - the inputNumber component
-	 *
-	 * @return {Object} an object containing properties for formatting the number
-	 */
-	getFormat: function(component) {
-		var precision = this.getPrecision(component);
-		var scale = this.getScale(component, precision);
-		var range = this.getRange(precision, scale);
-		var min = this.getMin(component, range);
-		var max = this.getMax(component, range);
-		var step = this.getStep(component, scale);
-		var nillable = this.utils.asBoolean(component.get('v.nillable'));
-
-		// When a user presses the ARROW UP or ARROW DOWN key in a number field the value is
-		// incremented/decremented by the amount in the step attribute. The min attribute affects
-		// this behavior. The native control assumes a min of 0 if one is not explicitly provided.
-		// If we use the min we calculated above when incrementing/decrementing by step then our
-		// component will not function exactly as the native element. To address this we will use
-		// this stepMin value which will be the value from the min attribute if one was supplied or
-		// 0 otherwise
-		var stepMin = this.toNumber(component.get('v.min'), 0);
-
-		return {
-			precision: precision,
-			scale: scale,
-			min: min,
-			max: max,
-			step: step,
-			stepMin: stepMin,
-			nillable: nillable
-		};
-	},
-
-	/**
-	 * Returns true if the character is a special multiplier shortcut
-	 *
-	 * @param {string} value - the value to test
-	 *
-	 * @return {boolean} true if value is a shortcut
-	 */
-	isMultiplier: function(value) {
-		return this.multipliers.indexOf(value.toLowerCase()) >= 0;
-	},
-
-	/**
-	 * Checks to see if the value ends with a special multiplier shortcut. These shortcuts include K
-	 * for thousands, M for millions, B for billions and T for trillions
-	 *
-	 * @param {string} value - the value to test
-	 *
-	 * @return {boolean} true if the value ends with a multiplier; otherwise, false
-	 */
-	hasMultiplier: function(value) {
-		for (var i = 0, n = this.multipliers.length; i < n; i++) {
-			var multiplier = this.multipliers[i];
-			if (this.utils.endsWithIgnoreCase(value, multiplier)) {
-				return true;
+		var self = this;
+		return action.call(behavior, event, {
+			select: function() {
+				inputElement.select();
+			},
+			getSelectionStart: function() {
+				return inputElement.selectionStart;
+			},
+			getSelectionEnd: function() {
+				return inputElement.selectionEnd;
+			},
+			getInputValue: function() {
+				return inputElement.value;
+			},
+			setInputValue: function(value) {
+				inputElement.value = value;
+			},
+			getValue: function() {
+				return component.get('v.value');
+			},
+			setValue: function(value) {
+				return self.setValue(component, value);
 			}
-		}
-		return false;
-	},
-
-	/**
-	 * Parses a string to a number. The returned number will conform to the constraints specified by
-	 * the format object. For example, if a string of "3,002" is passed in and the format object
-	 * specifies a max value of 99 then 99 will be returned. Conversely, if the string "15" was
-	 * passed in with the same format object then 15 would be returned
-	 *
-	 * @param {*}      value  - the value to be parsed. May be any type, but anything other than a
-	 *                          string will be converted to a string before parsing
-	 * @param {Object} format - an object that contains formatting information and constraints
-	 *
-	 * @return {number} the parsed number or undefined if the value could not be parsed
-	 */
-	parseNumber: function(value, format) {
-		value = this.utils.asNumber(value);
-		if (this.utils.isNumber(value)) {
-			value = value.toFixed(format.scale);
-			value = parseFloat(value);
-			value = this.utils.minmax(value, format.min, format.max);
-		} else {
-			value = format.nillable ? undefined : format.min;
-		}
-
-		return value;
-	},
-
-	/**
-	 * Formats a number using the information provided by a format object. If the value is not a
-	 * number then an empty string will be returned
-	 *
-	 * @param {number}  value      - the number to format
-	 * @param {Object}  format     - an object containing formatting information
-	 * @param {boolean} [hasFocus] - if true then the thousands separator will not be used
-	 *
-	 * @return {string} the formatted number
-	 */
-	formatNumber: function(value, format, hasFocus) {
-		var thousands = ',';
-		if (this.utils.isMobile() || hasFocus) {
-			thousands = '';
-		}
-
-		return this.utils.formatNumber(value, format.scale, thousands);
+		});
 	},
 
 	/**
@@ -307,16 +104,15 @@
 			return;
 		}
 
-		var format = this.getFormat(component);
-		var value = component.get('v.value');
-
-		if (this.utils.isNumber(value)) {
-			value = this.formatNumber(value, format, hasFocus);
-		} else {
-			value = this.utils.asString(value);
-		}
-
-		inputElement.value = value;
+		var behavior = component.find('behavior').getModule();
+		behavior.updateInputElement({
+			getValue: function() {
+				return component.get('v.value');
+			},
+			setInputValue: function(value) {
+				inputElement.value = value;
+			}
+		}, hasFocus);
 	},
 
 	/**
@@ -329,10 +125,10 @@
 	setInputElementAttributesForMobile: function(component) {
 		var inputElement = this.getInputElement(component);
 		if (inputElement && this.utils.isMobile()) {
-			var format = this.getFormat(component);
-			inputElement.min = format.min;
-			inputElement.max = format.max;
-			inputElement.step = format.step;
+			var behavior = component.find('behavior').getModule();
+			inputElement.min = behavior.getMin();
+			inputElement.max = behavior.getMax();
+			inputElement.step = behavior.getStep();
 		}
 	},
 
