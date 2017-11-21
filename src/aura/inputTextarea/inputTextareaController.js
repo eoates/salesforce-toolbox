da743819a9aa6414b85236a9bd5d1b5c4e988f63
Eugene Oates <eugene.oates@gmail.com>
2017-11-20 23:45:20 -0600

    Added inputTextarea component.

---
+++ src/aura/inputTextarea/inputTextareaController.js
@@ -0,0 +1,108 @@
+({
+	/**
+	 * Sets focus to the input element
+	 */
+	focus: function(component, event, helper) {
+		var inputElement = helper.getInputElement(component);
+		if (inputElement) {
+			inputElement.focus();
+		}
+	},
+
+	/**
+	 * Selects the input element
+	 */
+	select: function(component, event, helper) {
+		var inputElement = helper.getInputElement(component);
+		if (inputElement) {
+			inputElement.select();
+		}
+	},
+
+	/**
+	 * Initializes the component
+	 */
+	init: function(component, event, helper) {
+		helper.importModules(component);
+		helper.setOldValue(component);
+	},
+
+	/**
+	 * Handles change to the value attribute
+	 */
+	valueChange: function(component, event, helper) {
+		if (component.ignoreValueChange) {
+			return;
+		}
+
+		helper.setOldValue(component);
+		helper.updateInputElement(component);
+		helper.updateCounterElement(component);
+	},
+
+	/**
+	 * Handles the focus event of the input element
+	 */
+	inputFocus: function(component, event, helper) {
+		var inputElement = event.target;
+
+		var autoSelect = component.get('v.autoselect');
+		if (autoSelect) {
+			inputElement.select();
+		}
+
+		helper.fireEvent(component, 'onfocus');
+	},
+
+	/**
+	 * Handles the blur event of the input element
+	 */
+	inputBlur: function(component, event, helper) {
+		helper.fireEvent(component, 'onblur');
+	},
+
+	/**
+	 * Handles the input event of the input element
+	 */
+	inputInput: function(component, event, helper) {
+		var inputElement = event.target;
+		var value = inputElement.value;
+
+		var autoTrim = component.get('v.autotrim');
+		if (autoTrim) {
+			value = helper.utils.trim(value);
+		}
+
+		helper.setValue(component, value);
+		helper.updateCounterElement(component);
+	},
+
+	/**
+	 * Handles the change event of the input element
+	 */
+	inputChange: function(component, event, helper) {
+		var inputElement = event.target;
+		var value = inputElement.value;
+
+		var autoTrim = component.get('v.autotrim');
+		if (autoTrim) {
+			value = helper.utils.trim(value);
+			inputElement.value = value;
+		}
+
+		helper.updateCounterElement(component);
+
+		var oldValue = component.get('v.oldValue');
+		oldValue = helper.utils.asString(oldValue);
+
+		var changed = helper.setValue(component, value);
+		if (!changed) {
+			changed = value !== oldValue;
+		}
+
+		if (changed) {
+			helper.setOldValue(component);
+			helper.fireEvent(component, 'onchange');
+		}
+	}
+})
---
+++ src/aura/inputTextarea/inputTextareaHelper.js
@@ -0,0 +1,147 @@
+({
+	/**
+	 * Imports modules used by the component
+	 *
+	 * @param {Aura.Component} component - The inputTextarea component
+	 *
+	 * @return {void}
+	 */
+	importModules: function(component) {
+		if (!this.utils) {
+			this.utils = component.find('utils').getModule();
+		}
+	},
+
+	/**
+	 * Sets the oldValue to the current value
+	 *
+	 * @param {Aura.Component} component - The inputTextarea component
+	 *
+	 * @return {void}
+	 */
+	setOldValue: function(component) {
+		component.set('v.oldValue', component.get('v.value'));
+	},
+
+	/**
+	 * Sets the value
+	 *
+	 * @param {Aura.Component} component - The inputTextarea component
+	 * @param {string}         value     - The value
+	 *
+	 * @return {boolean} true if the value changed
+	 */
+	setValue: function(component, value) {
+		var oldValue = component.get('v.value');
+		if (value === oldValue) {
+			return false;
+		}
+
+		component.ignoreValueChange = true;
+		try {
+			component.set('v.value', value);
+		} finally {
+			component.ignoreValueChange = false;
+		}
+
+		return true;
+	},
+
+	/**
+	 * Returns the input HTML element
+	 *
+	 * @param {Aura.Component} component - The inputTextarea component
+	 *
+	 * @return {HTMLElement} The input element
+	 */
+	getInputElement: function(component) {
+		var input = component.find('input');
+		if (input) {
+			return input.getElement();
+		}
+		return undefined;
+	},
+
+	/**
+	 * Returns the span that displays the number of characters remaining
+	 *
+	 * @param {Aura.Component} component - The inputTextarea component
+	 *
+	 * @return {HTMLElement} The counter element
+	 */
+	getCounterElement: function(component) {
+		var counter = component.find('counter');
+		if (counter) {
+			return counter.getElement();
+		}
+		return undefined;
+	},
+
+	/**
+	 * Updates the value of the input element so it displays the formatted value
+	 *
+	 * @param {Aura.Component} component - The inputTextarea component
+	 *
+	 * @return {void}
+	 */
+	updateInputElement: function(component) {
+		var inputElement = this.getInputElement(component);
+		if (inputElement) {
+			var value = component.get('v.value');
+			value = this.utils.asString(value);
+
+			inputElement.value = value;
+		}
+	},
+
+	/**
+	 * Updates the span which displays the number of characters remaining. This should always be
+	 * called after updateInputElement
+	 *
+	 * @param {Aura.Component} component - The inputTextarea component
+	 *
+	 * @return {void}
+	 */
+	updateCounterElement: function(component) {
+		var inputElement = this.getInputElement(component);
+		var counterElement = this.getCounterElement(component);
+		if (inputElement && counterElement) {
+			var message = '';
+
+			var value = inputElement.value;
+			value = this.utils.asString(value);
+
+			var length = value.length;
+
+			var maxLength = component.get('v.maxlength');
+			if (this.utils.isInteger(maxLength)) {
+				var chars = '';
+				if (length > maxLength) {
+					chars = this.utils.formatNumber(length - maxLength, 0);
+					message = chars + ' characters too long';
+				} else if (maxLength >= 0) {
+					chars = this.utils.formatNumber(maxLength - length, 0);
+					message = chars + ' characters remaining';
+				}
+			}
+
+			counterElement.innerText = message;
+			counterElement.title = message;
+		}
+	},
+
+	/**
+	 * Fire the named event
+	 *
+	 * @param {Aura.Component} component - The inputTextarea component
+	 * @param {string}         name      - The event name
+	 * @param {Object}         args      - Optional event arguments
+	 *
+	 * @return {void}
+	 */
+	fireEvent: function(component, name, args) {
+		var event = component.getEvent(name);
+		event.setParam('arguments', args || {});
+		event.fire();
+	}
+})
